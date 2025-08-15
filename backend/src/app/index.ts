7ad6f98e41f4c76909e331fe17d0afa58480d0e3
ScorchedPearl <iit2024087@iiita.ac.in>
2025-08-15 23:44:20 +0530

    mongoose

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import DatabaseService from "./services/databaseservice";
+import cors from "cors";
 export const initServer: () => any = async () => {
+  const dbService = DatabaseService.getInstance();
+  await dbService.connect();
   const app=express();
+  app.use(cors({
+    origin: "*",
+    credentials: true
+  }));
   app.use(bodyParser.json())
+  // app.use('/api/auth', authRouter);
   return app;
 }
