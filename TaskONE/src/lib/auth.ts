0c7de9ccf3abb560de2ef7a20147707d3c9caed5
ScorchedPearl <iit2024087@iiita.ac.in>
2025-08-16 10:33:42 +0530

    New: UserContext

---
+++ TaskONE/src/lib/auth.ts
@@ -0,0 +1,36 @@
+import type { User } from "../types/auth_interface";
+import axios from "axios";
+
+export const currentUserFetcher = async (): Promise<User | null> => {
+  try {
+    const rawToken = localStorage.getItem('__Pearl_Token');
+    if (!rawToken) return null;
+
+    const token = `Bearer ${rawToken}`;
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/user/current-user`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    if (response.status === 200) {
+      const { name, email } = response.data;
+
+      return {
+        name,
+        email,
+        avatar: "/placeholder.svg", 
+      };
+    }
+    return null;
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return null;
+  }
+};
+
+
+
+
+
+
